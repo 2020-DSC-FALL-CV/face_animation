d39e0edb392e0590befa8fd5539a89fdcf2fc54b
hoyeon94 <hoyeon94@korea.com>
2020-12-21 23:09:32 +0900

    no message

--- web/front/babel.config.js
+++ web/front/babel.config.js
@@ -2,6 +2,5 @@ module.exports = {
     presets: [
         '@vue/cli-plugin-babel/preset'
     ],
-    plugins: ["transform-remove-console"]
 
 }
--- web/front/src/views/main/main.vue
+++ web/front/src/views/main/main.vue
@@ -1,70 +1,87 @@
 <template>
-	<div
-	>
-	<h1 style="text-align:center">CARTOON GAN!</h1>
-		<div style="display:flex;justify-content:center">
+<div>
+			<h1 style="text-align:center">CARTOON GAN!</h1>
+	<div style="display:flex;margin:2rem;">
+		<div style="width:50%;">
+			<div
+				style="display:flex;justify-content:center;align-items:center;flex-direction:column;"
+			>
+				<a-button style="margin:1rem" @click="visible = true">
+					이미지 추가
+				</a-button>
+				<img :src="imgSrc" style="width:500px;height:500px;border:0.2rem
+				solid var(--main-primary)" />
+				<a-button style="margin:1rem" v-if="clipSrc !== ''">
+					uploadButton
+				</a-button>
+			</div>
+		</div>
+		<div style="width:50%;display:flex;justify-content:center">
+					<div
+				style="display:flex;justify-content:center;align-items:center;flex-direction:column;"
+			>
+				<a-button style="margin:1rem;visibility: hidden;">
+					이미지 추가
+				</a-button>
+			<div style="width:500px;height:500px;background-color:black;"></div>
+							<a-button style="margin:1rem;visibility: hidden;" >
+					uploadButton
+				</a-button>
+		</div>
+		</div>
+		<a-modal
+			title="이미지 바꾸기!"
+			:visible="visible"
+			@ok="handleOk"
+			@cancel="
+				() => {
+					visible = false;
+				}
+			"
+		>
 			<clipper-upload v-model="clipSrc">
 				<a-button>
 					이미지 선택
 				</a-button>
 			</clipper-upload>
-		</div>
-		<div v-if="clipSrc !== ''">
-			<h4 style="text-align:center">
-				얼굴 영역을 선택해주세요!
-			</h4>
-			<div
-				class="flex"
-				style="margin : 1rem 0;display:flex;justify-content:center;align-items:center;flex-direction:column"
-			>
-				<div style="max-width:1000px;">
-					<clipper-basic
-						class="basic lg"
-						:src="clipSrc"
-						preview="preview"
-						ref="clipper"
-						:ratio="1"
-					></clipper-basic>
-				</div>
-				<div style="margin:1rem 0">
-					<h1 class="input-label">
-						선택된 영역
-					</h1>
-				</div>
+			<div v-if="clipSrc !== ''">
+				<h4 style="text-align:center">
+					얼굴 영역을 선택해주세요!
+				</h4>
 				<div
-					style="width:200px;height:200px;border:0.2rem solid var(--main-primary)"
+					class="flex"
+					style="margin : 1rem 0;display:flex;justify-content:center;align-items:center;flex-direction:column"
 				>
-					<clipper-preview
-						class="basic md"
-						name="preview"
-					></clipper-preview>
+					<div style="max-width:1000px;">
+						<clipper-basic
+							class="basic lg"
+							:src="clipSrc"
+							preview="preview"
+							ref="clipper"
+							:ratio="1"
+						></clipper-basic>
+					</div>
 				</div>
-		<div style="display:flex;justify-content:center;margin-top:1rem;">
-				<a-button>
-					얼굴 바꾸기!
-				</a-button>
-		</div>
 			</div>
-		</div>
+		</a-modal>
 	</div>
+</div>
 </template>
 <script>
-import { clipperBasic, clipperUpload, clipperPreview } from "vuejs-clipper";
+import { clipperBasic, clipperUpload } from "vuejs-clipper";
 import api from "@/api";
 export default {
 	data() {
 		return {
 			clipSrc: "",
+			visible: false,
+			fileObj:"",
+			imgSrc:""
 		};
 	},
 	components: {
 		clipperBasic,
 		clipperUpload,
-		clipperPreview
-	},
-	props: {
-		visible: Boolean,
-		handleCancel: Function
 	},
 	methods: {
 		async srcToFile(src, fileName, mimeType) {
@@ -75,29 +92,43 @@ export default {
 				.then(function(buf) {
 					return new File([buf], fileName, { type: mimeType });
 				});
-        },
-		async uploadHandlerTemp() {
-            try{
-                const canvas = this.$refs.clipper.clip(); //call component's clip method
-                const resultURL = canvas.toDataURL("image/jpeg", 1); //canvas->image
-                const formData = new FormData();
-                const fileObj = await this.srcToFile(resultURL,"file","image/jpeg");
-                formData.append("file", fileObj);
-                console.log("formData", formData);
-                await api.post("/upload/profile", formData, {
-                    headers: {
-                        "content-type": "multipart/form-data"
-                    }
-                })
-                alert("프로필 사진 수정이 완료되었습니다!");
-                this.$router.go(this.$router.currentRoute);
-            }
-            catch(err){
-                console.log(err);
-                alert("업로드 중 오류가 발생했습니다.");
-            }
-
 		},
+		async uploadHandlerTemp() {
+			try {
+				const canvas = this.$refs.clipper.clip(); //call component's clip method
+				const resultURL = canvas.toDataURL("image/jpeg", 1); //canvas->image
+				const formData = new FormData();
+				const fileObj = await this.srcToFile(
+					resultURL,
+					"file",
+					"image/jpeg"
+				);
+				formData.append("file", fileObj);
+				console.log("formData", formData);
+				await api.post("/upload/profile", formData, {
+					headers: {
+						"content-type": "multipart/form-data"
+					}
+				});
+				alert("프로필 사진 수정이 완료되었습니다!");
+				this.$router.go(this.$router.currentRoute);
+			} catch (err) {
+				console.log(err);
+				alert("업로드 중 오류가 발생했습니다.");
+			}
+		},
+		async handleOk() {
+			const canvas = this.$refs.clipper.clip(); //call component's clip method
+			const resultURL = canvas.toDataURL("image/jpeg", 1); //canvas->image
+			const fileObj = await this.srcToFile(
+				resultURL,
+				"file",
+				"image/jpeg"
+			);
+			this.fileObj = fileObj;
+			this.imgSrc = resultURL; 
+			this.visible = false;
+		}
 	}
 };
 </script>
